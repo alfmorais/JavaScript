c41a5c44c9ad5faf77f32f982651992d7e39256c
alfmorais <alfredneto@hotmail.com>
2023-07-23 09:57:08 -0300

    Feat: exercises about data in JS

---
+++ introduction/_exerciseFour/script.js
@@ -0,0 +1,3 @@
+console.log(2 === 2 && 3 != 6);
+console.log(2 >= 2 || 3 != 3);
+console.log(!(2 === 2 && 3 != 6));
---
+++ introduction/_exerciseOne/script.js
@@ -0,0 +1,3 @@
+console.log("Um simples string com aspas duplas...");
+console.log('Um simples string com aspas simples...');
+console.log(`2 + 2 = ${2+2} e não 5 iguais aos uns ditadores dizem...`);
