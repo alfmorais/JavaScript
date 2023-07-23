af4b30feca35ccf9a949dfa8877b69a6aa1aac87
alfmorais <alfredneto@hotmail.com>
2023-07-23 15:07:18 -0300

    Feat: basics about variables and built-in functions

---
+++ structureProgramming/builtin/script.js
@@ -0,0 +1,22 @@
+// PROMPT
+let age = prompt("Qual é sua idade?");
+console.log(`Sua idade é ${age}`);
+
+// ALERT
+alert("Veja essa mensagem abestado!");
+
+// MATH
+let maxNumber = Math.max(1, 5, 2, 3);
+alert(`O maior número é: ${maxNumber}`);
+
+let minNumber = Math.max(1, 5, 2, 3);
+alert(`O menor número é: ${minNumber}`);
+
+let number = Math.round(5.2568);
+console.log(number)
+
+let numberTwo = Math.ceil(5.2568);
+console.log(numberTwo)
+
+// CONSOLE.LOG
+console.log("Função")
---
+++ structureProgramming/variables/script.js
@@ -0,0 +1,15 @@
+let laranjas = 5;
+console.log(`A cesta tem ${laranjas} laranjas.`);
+
+let nome = "Alfredo";
+console.log(`Meu nome é ${nome}.`)
+
+console.log(laranjas * laranjas);
+
+nome = "Joaquim";
+console.log(nome);
+
+// Outras formas de declarar variaveis
+const name = "Joaquim";
+var lastName = "Morais";
+console.log(name + " " + lastName);
